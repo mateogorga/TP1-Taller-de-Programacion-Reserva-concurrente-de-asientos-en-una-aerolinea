3c3ec1cc262d9203d87eb3d22cf6e8c31791d7d7
mateogorga <mgorga@fi.uba.ar>
2026-04-26 16:16:40 -0300

    test

---
+++ lib/condor_del_sur.ex
@@ -0,0 +1,18 @@
+defmodule CondorDelSur do
+  @moduledoc """
+  Documentation for `CondorDelSur`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CondorDelSur.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/condor_del_sur_test.exs
@@ -0,0 +1,8 @@
+defmodule CondorDelSurTest do
+  use ExUnit.Case
+  doctest CondorDelSur
+
+  test "greets the world" do
+    assert CondorDelSur.hello() == :world
+  end
+end
