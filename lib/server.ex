e5e8a35e3517547e3a98024eca2f680d669571c5
mateogorga <mgorga@fi.uba.ar>
2026-05-02 16:50:49 -0300

    Se incluyeron las siguientes funcionalidades: reservar asiento, confirmar y cancelar reserva, manejo de expiracion y concurrencia, uso de monitor

--- lib/server.ex
+++ lib/server.ex
@@ -1,8 +1,8 @@
 defmodule CondorDelSur.FlightServer do
+  @reservation_timeout 5_000
+
   def start do
-    pid = spawn(fn -> loop(%{seats: %{}, reservations: %{}}) end)
-    Process.register(pid, :flight_server)
-    pid
+    spawn(fn -> loop(%{seats: %{}, reservations: %{}}) end)
   end
 
   defp loop(state) do
@@ -17,8 +17,131 @@ defmodule CondorDelSur.FlightServer do
           |> Enum.map(fn id -> {id, %CondorDelSur.Seat{id: id}} end)
           |> Enum.into(%{})
 
-        new_state = %{state | seats: new_seats}
+        new_state = %{
+          state
+          | seats: Map.merge(state.seats, new_seats)
+        }
         loop(new_state)
+
+      {:reserve, passenger, seat_id, caller} ->
+        case Map.get(state.seats, seat_id) do
+          %CondorDelSur.Seat{status: :available} ->
+            reservation_id = :erlang.unique_integer([:positive])
+
+            reservation = %CondorDelSur.Reservation{
+              id: reservation_id,
+              passenger: passenger,
+              seat_id: seat_id,
+              status: :pending
+            }
+
+            updated_seat = %CondorDelSur.Seat{state.seats[seat_id] | status: :reserved}
+
+            server = self()
+
+            pid = spawn(fn ->
+              :timer.sleep(@reservation_timeout)
+              send(server, {:expire, reservation_id})
+            end)
+
+            Process.monitor(pid)
+
+            new_state = %{
+              state
+              | seats: Map.put(state.seats, seat_id, updated_seat),
+                reservations: Map.put(state.reservations, reservation_id, reservation)
+            }
+
+            send(caller, {:ok, reservation})
+            loop(new_state)
+
+          %CondorDelSur.Seat{} ->
+            send(caller, {:error, :seat_not_available})
+            loop(state)
+
+          nil ->
+            send(caller, {:error, :seat_not_found})
+            loop(state)
+        end
+
+      {:confirm_reservation, reservation_id, caller} ->
+        case Map.get(state.reservations, reservation_id) do
+          %CondorDelSur.Reservation{status: :pending} = reservation ->
+
+            updated_reservation =
+              %CondorDelSur.Reservation{
+                reservation | status: :confirmed
+              }
+
+            updated_seat =
+              %CondorDelSur.Seat{
+                state.seats[reservation.seat_id]
+                | status: :confirmed
+              }
+
+            new_state = %{
+              state
+              | seats: Map.put(state.seats, reservation.seat_id, updated_seat),
+                reservations: Map.put(state.reservations, reservation_id, updated_reservation)
+            }
+
+            send(caller, {:ok, updated_reservation})
+            loop(new_state)
+
+          %CondorDelSur.Reservation{} ->
+            send(caller, {:error, :cannot_confirm})
+            loop(state)
+
+          nil ->
+            send(caller, {:error, :reservation_not_found})
+            loop(state)
+        end
+
+      {:cancel_reservation, reservation_id, caller} ->
+        case Map.get(state.reservations, reservation_id) do
+          %CondorDelSur.Reservation{status: :pending} = reservation ->
+            updated_reservation = %CondorDelSur.Reservation{reservation | status: :cancelled}
+            updated_seat = %CondorDelSur.Seat{state.seats[reservation.seat_id] | status: :available}
+
+            new_state = %{
+              state
+              | seats: Map.put(state.seats, reservation.seat_id, updated_seat),
+                reservations: Map.put(state.reservations, reservation_id, updated_reservation)
+            }
+
+            send(caller, {:ok, updated_reservation})
+            loop(new_state)
+
+          %CondorDelSur.Reservation{} ->
+            send(caller, {:error, :cannot_cancel})
+            loop(state)
+
+          nil ->
+            send(caller, {:error, :reservation_not_found})
+            loop(state)
+        end
+
+      {:expire, reservation_id} ->
+        case Map.get(state.reservations, reservation_id) do
+          %CondorDelSur.Reservation{status: :pending} = reservation ->
+            updated_reservation = %CondorDelSur.Reservation{reservation | status: :expired}
+            updated_seat = %CondorDelSur.Seat{state.seats[reservation.seat_id] | status: :available}
+
+            new_state = %{
+              state
+              | seats: Map.put(state.seats, reservation.seat_id, updated_seat),
+                reservations: Map.put(state.reservations, reservation_id, updated_reservation)
+            }
+
+            loop(new_state)
+
+          _ ->
+            loop(state)
+        end
+
+      {:DOWN, _ref, :process, _pid, reason} ->
+        IO.puts("Proceso monitoreado terminó: #{inspect(reason)}")
+        loop(state)
 
       _ ->
         loop(state)
@@ -31,5 +154,10 @@ defmodule CondorDelSur.FlightServer do
     receive do
       {:state, state} -> state
     end
+  end
+
+  def  available_seats(server) do
+    state = get_state(server)
+    state.seats |> Enum.filter(fn {_id, seat} -> seat.status == :available end) |> Enum.map(fn {id, _} -> id end)
   end
 end
--- test/flight_server_test.exs
+++ test/flight_server_test.exs
@@ -14,4 +14,91 @@ defmodule FlightServerTest do
     assert state.seats[1].status == :available
     assert state.seats[2].status == :available
   end
+
+  test "reserva un asiento disponible" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+
+    assert_receive {:ok, reservation}
+    assert reservation.passenger == "Mateo Gorga"
+    assert reservation.seat_id == 1
+    assert reservation.status == :pending
+  end
+
+  test "no puede reservar un asiento ocupado" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+    assert_receive {:ok, _}
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+    assert_receive {:error, :seat_not_available}
+  end
+
+  test "confirma una reserva pendiente" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+    send(pid, {:reserve, "Juan", 1, self()})
+    assert_receive {:ok, reservation}
+
+    send(pid, {:confirm_reservation, reservation.id, self()})
+    assert_receive {:ok, updated}
+
+    assert updated.status == :confirmed
+
+    state = FlightServer.get_state(pid)
+    assert state.seats[1].status == :confirmed
+  end
+
+  test "cancela una reserva pendiente" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+    assert_receive {:ok, reservation}
+
+    send(pid, {:cancel_reservation, reservation.id, self()})
+    assert_receive {:ok, cancelled_reservation}
+    assert cancelled_reservation.status == :cancelled
+  end
+
+  test "no cancela una reserva confirmada" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+    assert_receive {:ok, reservation}
+
+    send(pid, {:confirm_reservation, reservation.id, self()})
+    assert_receive {:ok, _}
+
+    send(pid, {:cancel_reservation, reservation.id, self()})
+    assert_receive {:error, :cannot_cancel}
+  end
+
+  test "expira una reserva pendiente" do
+    pid = FlightServer.start()
+
+    send(pid, {:create_seats, [1]})
+
+    send(pid, {:reserve, "Mateo Gorga", 1, self()})
+    assert_receive {:ok, reservation}
+
+    :timer.sleep(6_000)
+
+    state = FlightServer.get_state(pid)
+    expired_reservation = state.reservations[reservation.id]
+
+    assert expired_reservation.status == :expired
+    assert state.seats[1].status == :available
+  end
+
 end
